42091412bdea2968d309cb5d4da3598dca29bf26
Hayder-Suwaed <haiderhachim87@yahoo.com>
2019-10-23 20:57:20 +0000

    Move Commands

--- client.js
+++ client.js
@@ -5,19 +5,26 @@ const net = require('net');
  */
 const connect = function() {
     const conn = net.createConnection({ 
-    //    host: '192.168.15.225',
-      host: 'localhost',
+       host: '192.168.15.225',
+    //   host: 'localhost',
       port: 50541
     });
+    conn.on("data", data => {
+        console.log("Server says: ", data);
+    });
     // interpret incoming data as text
-    conn.setEncoding('utf8'); 
   
       conn.on('connect', () => {
       console.log('Successfully connected to game server');
-      conn.write("name: hhh")
+      conn.write("Name: JUD")
+          let m = setInterval(() => {
+        conn.write("Move: up");
+            conn.write("Move: right");
+      }, 500);
+      setTimeout(() => clearInterval(m), 2000);
     });
-  
+    conn.setEncoding('utf8'); 
     return conn;
   }
   
-   module.exports = connect
+   module.exports = connect;
