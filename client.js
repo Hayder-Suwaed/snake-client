cab7e3a69155b7e209359b820f863d016d48c643
Hayder-Suwaed <haiderhachim87@yahoo.com>
2019-10-23 20:18:54 +0000

    Name Command

--- client.js
+++ client.js
@@ -5,15 +5,16 @@ const net = require('net');
  */
 const connect = function() {
     const conn = net.createConnection({ 
-      // host: '192.168.15.225',
+    //    host: '192.168.15.225',
       host: 'localhost',
       port: 50541
     });
     // interpret incoming data as text
     conn.setEncoding('utf8'); 
   
-      conn.on('data', (data) => {
-      console.log('Server says: ', data);
+      conn.on('connect', () => {
+      console.log('Successfully connected to game server');
+      conn.write("name: hhh")
     });
   
     return conn;
